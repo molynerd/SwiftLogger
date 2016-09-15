720606dbf801232de035ed0a44cd323d90c97f2c
nmolyneux <nickneux21@gmail.com>
2016-09-14 17:00:55 -0700

    Initial Commit

---
+++ SwiftLogger/SwiftLogger.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLogger.h
+//  SwiftLogger
+//
+//  Created by Nicholas Molyneux on 9/14/16.
+//  Copyright © 2016 Nicholas Molyneux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLogger.
+FOUNDATION_EXPORT double SwiftLoggerVersionNumber;
+
+//! Project version string for SwiftLogger.
+FOUNDATION_EXPORT const unsigned char SwiftLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLogger/PublicHeader.h>
+
+
